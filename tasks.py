41fcd9c0253b0199f87f7696d1816103079da679
liuxinwei <liuxinwei@xmsilicon.cn>
2025-09-18 11:34:56 +0800

    fix: 更新导入路径以使用正确的模块
    将taolib.flows.tasks.sites的导入路径更新为taolib.doc.sites，以修复模块引用错误

--- tasks.py
+++ tasks.py
@@ -3,6 +3,6 @@ import inspect
 if not hasattr(inspect, 'getargspec'): # 修复
     inspect.getargspec = inspect.getfullargspec
     
-from taolib.flows.tasks import sites
+from taolib.doc import sites
 
 namespace = sites('docs', target='docs/_build/html')
